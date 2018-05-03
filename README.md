3da749252848f0b00922555b82bdb0f44d219f81
Conor Morrison <conor.morrison@hilton.com>
2018-05-03 17:48:42 +0100

    started readme, pushed up dev & min files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # slothLoader
 Another lazyloading plugin from an uninspired mind.
+
+## blah
+Add the plugin code to your site. Then call slothLoader like:
+```
+$('body').slothLoader({
+    'offset': $(window).height(), // adjust this value to whatever you want
+    'elem': '.lazy', // looks for this class to lazy load [default = .lazy-loadable]
+    'dataSrc': 'lazy' // define own data attr [default = lazy]
+});
+```
+
+Your HTML should be similar to:
+
+TBC
---
+++ dist/slothLoader.jquery.js
@@ -0,0 +1,72 @@
+;( function( $, window, document, undefined ) {
+
+		"use strict";
+
+		var pluginName = "slothLoader",
+			defaults = {
+				'offset': $(window).height(), //how long before it kicks in
+		    'elem': '.lazy-loadable', //looks for this class to lazy load
+		    'dataSrc': 'lazy' //define own data attr
+			};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+			this.element = element;
+
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend( Plugin.prototype, {
+			init: function() {
+	      $(window).scroll( $.proxy( this._sloth, this ) );
+	      $(window).load( $.proxy( this._sloth, this ) );
+			},
+
+			_sloth: function(){
+			 var scrollDist = $(window).scrollTop() + this.settings.offset;
+			 var el = $(this.settings.elem);
+       var self = this;
+			 el.each(function(){
+				 if( !$(this).hasClass('loaded') ){
+					 var scrollTop = $(this).offset().top;
+					 var dataImage = $(this).data(self.settings.dataSrc);
+					 //look for element type - img, source or everything else
+					 var elemType = this.localName;
+					 if( scrollDist > scrollTop ){
+						 if(elemType == 'img'){
+							 //check to see if image has srcset val & lazyload that instead
+							 if( $(this).attr('srcset') != undefined ){
+								 $(this).attr('srcset', dataImage);
+								 $(this).addClass('loaded');
+							 } else {
+								 $(this).attr('src', dataImage);
+								 $(this).addClass('loaded');
+							 }
+						 } else if(elemType == 'source') {
+							 $(this).attr('srcset', dataImage);
+							 $(this).addClass('loaded');
+						 } else {
+							 $(this).css('backgroundImage', 'url(' + dataImage + ')');
+							 $(this).addClass('loaded');
+						 }
+					 }
+				 }
+			 });
+		 }
+
+		} );
+
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			} );
+		};
+
+} )( jQuery, window, document );
